c36d441956649fb9eb1a3f3732a3b57d5867f71c
HanselD <hansel96@gmail.com>
2021-01-15 23:36:23 -0500

    added constants

--- app.py
+++ app.py
@@ -8,6 +8,22 @@ from flask import Flask, jsonify, redirect, render_template, url_for
 from authlib.integrations.flask_client import OAuth
 from six.moves.urllib.parse import urlencode
 
-app = Flask(__name__)
+# define constants
 
+import constants
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+AUTH0_CALLBACK_URL = env.get(constants.AUTH0_CALLBACK_URL)
+AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
+AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
+AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
+AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
+AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
+
+# build Flask app
+app = Flask(__name__)
+# build Oauth class
 OAuth = OAuth(app)
