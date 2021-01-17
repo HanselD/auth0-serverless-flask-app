eeba990bde6127e5e3230f7217a368fbe8ccfb6b
HanselD <hansel96@gmail.com>
2021-01-16 23:17:07 -0500

    new config and working routing

--- app.py
+++ app.py
@@ -2,43 +2,28 @@ from functools import wraps
 import json
 from os import environ as env
 from werkzeug.exceptions import HTTPException
-
+from flask_oidc import OpenIDConnect
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask, jsonify, redirect, render_template, url_for
-from authlib.integrations.flask_client import OAuth
 from six.moves.urllib.parse import urlencode
-
-# define constants
-
-import constants
-
-ENV_FILE = find_dotenv()
-if ENV_FILE:
-    load_dotenv(ENV_FILE)
-
-AUTH0_CALLBACK_URL = env.get(constants.AUTH0_CALLBACK_URL)
-AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
-AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
-AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
-AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
 
 # build Flask app
 app = Flask(__name__)
-# build Oauth class
-OAuth = OAuth(app)
+# build OIDC object
+app.config.update({
+    'SECRET_KEY': 'SomethingNotEntirelySecret',    
+    'OIDC_CLIENT_SECRETS': 'client_secrets.json',
+    'OIDC_ID_TOKEN_COOKIE_SECURE': False,       
+})
 
-auth0 = oauth.register(
-    'auth0',
-    client_id=AUTH0_CLIENT_ID,
-    client_secret=AUTH0_CLIENT_SECRET,
-    api_base_url=AUTH0_BASE_URL,
-    access_token_url=AUTH0_BASE_URL + '/oauth/token',
-    authorize_url=AUTH0_BASE_URL + '/authorize',
-    client_kwargs={
-        'scope': 'openid profile email',
-    },
-)
+oidc = OpenIDConnect(app)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/login')
+@oidc.require_login
+def login():
+    user = {"email": oidc.user_getfield('email')}
+    return render_template('home.html', user=user)
