562334d0b9440098e81b12afeaf3e1363592c1f4
HanselD <hansel96@gmail.com>
2021-01-30 20:17:27 -0500

    added main

--- app.py
+++ app.py
@@ -29,3 +29,6 @@ def login():
 def logout():
     oidc.logout()
     return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000) #Remove debug=True if deploying to Prod
